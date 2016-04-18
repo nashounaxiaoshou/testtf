2d413a27de6741c2c74c70f871e522f52353b81c
tianyuzhen <1148008482@qq.com>
2016-04-18 16:32:54 +0800

    第6章完结

--- New Unity Project 3/Assets/scripts/coconutwin.js
+++ New Unity Project 3/Assets/scripts/coconutwin.js
@@ -16,9 +16,12 @@ var tree_down:float=0;
 
 function Update () {
 	// 如果有三个倒下了 并且当前胜利状态为 false
-	if(targets==3 && havewon==false){
+	//if(targets==3 && havewon==false){
+	if(defen>=1000 && havewon==false){
+		
 		// 重置击倒数量
 		//targets=0;
+		
 		// 播放胜利
 		audio.PlayOneShot(winsound);
 		// 找到电池
@@ -31,11 +34,12 @@ function Update () {
 		Destroy(wincell);
 		// 状态改为胜利
 		havewon = true;
-		if(fenshu_bl.enabled==false){
-			fenshu_bl.enabled=true;
-		}
+		
+		//if(fenshu_bl.enabled==false){
+		//	fenshu_bl.enabled=true;
+		//}
 	}
-	if(fenshu_bl.enabled==true){
+	//if(fenshu_bl.enabled==true){
 		// 如果3个都是倒下状态
 		if(targets==3){
 			// 记录倒下的时间
@@ -46,10 +50,27 @@ function Update () {
 				targetcollision.resettime-=0.1;
 				// 得分加
 				defen+=100;
+				
 				// 重置倒下的时间
 				tree_down=0;
+				/*
+				if(defen>=1000){
+					// 播放胜利
+					audio.PlayOneShot(winsound);
+					// 找到电池
+					var wincell:GameObject = transform.FindChild("powerCell").gameObject;
+					// 移动电池 Translate:移动函数
+					wincell.transform.Translate(-1,0,0);
+					// 创建预制电池，并且放到指定的位置和旋转方向
+					Instantiate(cellprefab,wincell.transform.position,wincell.transform.rotation);
+					// 销毁原来的电池
+					Destroy(wincell);
+					// 状态改为胜利
+					havewon = true;
+				}*/
+				fenshu_bl.text = "得分:"+defen;
 			}
-		}
+		//}
 	}
-	fenshu_bl.text = "得分:"+defen;
+	//fenshu_bl.text = "得分:"+defen;
 }
