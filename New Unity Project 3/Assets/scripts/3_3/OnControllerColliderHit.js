9cb5be89c7e122497256be1c78c9c665de15a957
tianyuzhen <1148008482@qq.com>
2016-04-18 20:29:10 +0800

    u3d学习

---
+++ New Unity Project 3/Assets/scripts/3_3/OnControllerColliderHit.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+// OnControllerColliderHit(hit:ControllerColliderHit) 角色控制器专用函数
+function OnControllerColliderHit(hit:ControllerColliderHit){
+	if(hit.gameObject.name!="Plane"){
+		// 显示碰撞物体的名字
+		gameObject.Find("wenzi").guiText.text = hit.gameObject.name;
+	}
+}
