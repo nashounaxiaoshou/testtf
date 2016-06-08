cb7fbdfe390ae3430038f1f4af61a4e1f88e4e0d
tianyuzhen <1148008482@qq.com>
2016-06-08 17:51:42 +0800

    第8章第1讲

---
+++ New Unity Project 3/Assets/scripts/301/menu_play.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var leveltoload:String;
+// 一般情况下的纹理
+var normaltexture:Texture2D;
+//
+var rollovertexture:Texture2D;
+//
+var beep:AudioClip;
+//	鼠标悬浮时执行的方法
+function OnMouseEnter(){
+	guiTexture.texture = rollovertexture;
+}
+// 鼠标离开时执行的方法
+function OnMouseExit(){
+	guiTexture.texture = normaltexture;
+}
+// 鼠标按下时执行的方法
+function OnMouseDown(){
+	// 播放音效
+	audio.PlayOneShot(beep);
+	yield new WaitForSeconds(0.35);
+	Application.LoadLevel(leveltoload);
+}
