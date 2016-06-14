d89757cb8862bb078e993589d56d54ce01885018
tianyuzhen <1148008482@qq.com>
2016-06-14 14:19:32 +0800

    no message

--- New Unity Project 3/Assets/scripts/301/menu_play.js
+++ New Unity Project 3/Assets/scripts/301/menu_play.js
@@ -3,9 +3,9 @@
 var leveltoload:String;
 // 一般情况下的纹理
 var normaltexture:Texture2D;
-//
+// 鼠标指向时显示的纹理
 var rollovertexture:Texture2D;
-//
+// 引用音屏文件
 var beep:AudioClip;
 //	鼠标悬浮时执行的方法
 function OnMouseEnter(){
@@ -20,5 +20,6 @@ function OnMouseDown(){
 	// 播放音效
 	audio.PlayOneShot(beep);
 	yield new WaitForSeconds(0.35);
+	// 载入场景
 	Application.LoadLevel(leveltoload);
 }
